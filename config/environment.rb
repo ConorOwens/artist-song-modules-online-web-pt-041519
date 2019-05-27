7d6e051d5efc2d7fd0aa2932c7bdbc7adcfee16a
Conor Owens <conorj.owens@gmail.com>
2019-05-27 17:34:33 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -7,16 +7,3 @@ require 'pry'
 require_relative '../lib/artist.rb'
 require_relative '../lib/song.rb'
 
-module Memorable 
-    
-    def reset_all
-      all.clear
-    end
-    
-    def count
-      all.count
-    end
-    
-  end
-  
-end
